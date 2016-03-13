2e8abe900d2159d7bafd46b3f7baa37d83cd4fed
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2016-03-13 18:28:43 +0530

    Adding Students and Student by name API

--- src/main/java/com/asb/rxjava/controller/rest/StudentController.java
+++ src/main/java/com/asb/rxjava/controller/rest/StudentController.java
@@ -5,9 +5,7 @@ import com.asb.rxjava.model.eo.Student;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.inject.Inject;
@@ -17,8 +15,9 @@ import java.util.List;
  * Student Controller.
  * Created by arjun on 05/03/16.
  */
+@CrossOrigin(origins = {"http://localhost:8000"})
 @RestController
-@RequestMapping("/api/students")
+@RequestMapping("/api/student")
 public class StudentController {
 
     /**
@@ -32,13 +31,37 @@ public class StudentController {
      */
     private Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
 
+    /**
+     * Gets all the students.
+     *
+     * @return List of students
+     */
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public DeferredResult<Student> getStudentByName(@PathVariable("name") String name) {
+        LOGGER.info("Requesting in " + Thread.currentThread().getName() + " man");
+        DeferredResult<Student> deferredResult = new DeferredResult<>(200000L);
+        studentService.getStudentByName(name).subscribe(student -> {
+            deferredResult.setResult(student);
+            LOGGER.info("Collected result from " + Thread.currentThread().getName() + " man");
+        }, throwable -> {
+            deferredResult.setErrorResult(throwable);
+            LOGGER.error("Exception from " + Thread.currentThread().getName() + " man", throwable);
+        }, () -> {
+            if (!deferredResult.hasResult()) {
+                deferredResult.setResult(null);
+                LOGGER.warn("No result from " + Thread.currentThread().getName() + " man");
+            }
+        });
+        LOGGER.info("Exiting from " + Thread.currentThread().getName() + " man");
+        return deferredResult;
+    }
 
     /**
      * Gets all the students.
      *
      * @return List of students
      */
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public DeferredResult<List<Student>> getStudents() {
         LOGGER.info("Requesting in " + Thread.currentThread().getName() + " man");
         DeferredResult<List<Student>> deferredResult = new DeferredResult<>(200000L);
--- src/main/java/com/asb/rxjava/controller/service/StudentService.java
+++ src/main/java/com/asb/rxjava/controller/service/StudentService.java
@@ -5,6 +5,7 @@ import com.asb.utils.Sleeper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Observable;
+import rx.Subscriber;
 import rx.schedulers.Schedulers;
 
 import javax.inject.Named;
@@ -26,20 +27,46 @@ public class StudentService {
     private Random random = new Random();
     private List<String> NAMES = Arrays.asList("Arjun", "Aditya", "Saurab", "Gunjan", "Ashutosh", "Pooja", "Ahsan", "Madhu", "Azad", "Manju");
 
+    /**
+     * Get the observable of Students
+     *
+     * @return observable of students
+     */
     public Observable<Student> getStudents() {
         return Observable.<Student>create(
                 subscriber -> {
-                    NAMES.parallelStream().forEach(name ->
-                            {
-                                final int age = random.nextInt(20);
-                                LOGGER.info(name + " will be built in " + age + " seconds.");
-                                Sleeper.sleep(age * 1000);
-                                subscriber.onNext(Student.builder().name(name).age(age).build());
-                                LOGGER.info(name + " is built.");
-                            }
-                    );
+                    NAMES.parallelStream()
+                            .forEach(name -> createStudent(subscriber, name));
                     subscriber.onCompleted();
                     LOGGER.info("Completed the request.");
                 }).subscribeOn(Schedulers.from(executorService));
     }
+
+    /**
+     * Get the Student by name
+     *
+     * @param name name of the Student
+     * @return Observable of students
+     */
+    public Observable<Student> getStudentByName(String name) {
+        return Observable.<Student>create(
+                subscriber -> {
+                    NAMES.parallelStream()
+                            .filter(s -> s.equals(name))
+                            .findFirst()
+                            .ifPresent(studentName -> createStudent(subscriber, studentName));
+                    subscriber.onCompleted();
+                    LOGGER.info("Completed the request.");
+                }
+        ).subscribeOn(Schedulers.from(executorService));
+    }
+
+    private void createStudent(Subscriber<? super Student> subscriber, String studentName) {
+        final int age = random.nextInt(20);
+        LOGGER.info(studentName + " will be built in " + age / 10 + " seconds.");
+        Sleeper.sleep(age * 100);
+        subscriber.onNext(Student.builder().name(studentName).age(age).build());
+        LOGGER.info(studentName + " is built.");
+    }
+
 }
